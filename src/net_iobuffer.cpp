35faba5574d07d1d4f969cef1d23e7bab4c716ba
lw000 <373102227@qq.com>
2017-10-16 11:54:02 +0800

    Signed-off-by: lw000 <373102227@qq.com> 更新

--- src/net_iobuffer.cpp
+++ src/net_iobuffer.cpp
@@ -26,12 +26,9 @@ int NetIOBuffer::send(int cmd, void* object, int objectSize, std::function<int(N
 		netMsg.cmd = cmd;
 		netMsg.buf = msg->getBuf();
 		netMsg.size = msg->getSize();
-
 		{
-/*			lw_lock_guard l(&_wlock);*/
 			c = func(&netMsg);
 		}
-
 		delete msg;
 	}
 	return c;
@@ -54,60 +51,75 @@ int NetIOBuffer::parse(const char * buf, int size, PARSE_DATA_CALLFUNC call, voi
 		return -2;
 	}
 
-/*	clock_t t = clock();*/
-
+	int ret = -3;
+	clock_t t = clock();
+	
 	{
-		int cache_size = 0;
-		{
-			lw_lock_guard l(&_rlock);
-			_cacheQueue.push(const_cast<char*>(buf), size);
-			cache_size = (int)_cacheQueue.size();
+		lw_lock_guard l(&_rlock);
+		_cacheQueue.push(const_cast<char*>(buf), size);
+		int cacheLength = (int)_cacheQueue.size();
 
-			if (C_NETHEAD_SIZE <= cache_size) {
-				do
-				{				
-					char * package = nullptr;
-					NetHead *nh = (NetHead*)_cacheQueue.front();
-					if (cache_size < nh->size) {
-						LOGFMTD("not a complete data packet [cache_size:%d, head_size:%d, cmd:%d]", cache_size, nh->size, nh->cmd);
-						break;
-					}
-
-					package = new char[nh->size];
-					_cacheQueue.front_copyto(package, nh->size);
-					_cacheQueue.pop(nh->size);
-					cache_size = (int)_cacheQueue.size();
-
-					NetHead *phd = (NetHead*)package;
-
-					if (phd->size < 0) {
-						LOGD(phd->debug());
-					}
-
-					NetPackage* nmsg = new NetPackage(phd);
-					if (nullptr != nmsg) {
-						char* tbuf = &package[C_NETHEAD_SIZE];
-						int tbuf_len = phd->size - C_NETHEAD_SIZE;
-						nmsg->setMessage(tbuf, tbuf_len);
-						call(nmsg->getHead()->cmd, nmsg->getBuf(), nmsg->getSize(), userdata);
-					}
-					delete nmsg;
-
-					if (package != nullptr) {
-						delete package;
-					}
-				} while (cache_size >= C_NETHEAD_SIZE);
-			}
-			else {
-				LOGFMTD("not a complete data packet [cache_size:%d head_size:%d]", cache_size, C_NETHEAD_SIZE);
-			}
+		if (cacheLength < C_NETHEAD_SIZE) {
+			return -3;
+			LOGFMTD("not a complete data packet [cache_size:%d head_size:%d]", cacheLength, C_NETHEAD_SIZE);
 		}
+		
+		do
+		{
+			NetHead *nh = (NetHead*)_cacheQueue.front();
+			
+			if (cacheLength < nh->size) {
+				//LOGFMTD("not a complete data packet [cache_size:%d, head_size:%d, cmd:%d]", cache_size, nh->size, nh->cmd);
+				break;
+			}
+
+			NetPackage* nmsg = new NetPackage(nh);
+			if (nullptr != nmsg) {
+				char* buf = _cacheQueue.front();
+				char* tbuf = &buf[C_NETHEAD_SIZE];
+				int tbuf_len = nh->size - C_NETHEAD_SIZE;
+				nmsg->setMessage(tbuf, tbuf_len);
+				call(nmsg->getHead()->cmd, nmsg->getBuf(), nmsg->getSize(), userdata);
+			}
+			delete nmsg;
+			
+			_cacheQueue.pop(nh->size);
+			cacheLength = (int)_cacheQueue.size();
+		} while (cacheLength >= C_NETHEAD_SIZE);
+		
+
+// 		if (C_NETHEAD_SIZE <= cache_size) {
+// 			do
+// 			{				
+// 				NetHead *nh = (NetHead*)_cacheQueue.front();
+// 				if (cache_size < nh->size) {
+// 					//LOGFMTD("not a complete data packet [cache_size:%d, head_size:%d, cmd:%d]", cache_size, nh->size, nh->cmd);
+// 					break;
+// 				}
+// 
+// 				NetPackage* nmsg = new NetPackage(nh);
+// 				if (nullptr != nmsg) {
+// 					char* buf = _cacheQueue.front();
+// 					char* tbuf = &buf[C_NETHEAD_SIZE];
+// 					int tbuf_len = nh->size - C_NETHEAD_SIZE;
+// 					nmsg->setMessage(tbuf, tbuf_len);
+// 					call(nmsg->getHead()->cmd, nmsg->getBuf(), nmsg->getSize(), userdata);
+// 				}
+// 				delete nmsg;
+// 				_cacheQueue.pop(nh->size);
+// 				cache_size = (int)_cacheQueue.size();
+// 			} while (cache_size >= C_NETHEAD_SIZE);
+// 		}
+// 		else {
+// 			ret = -3;
+// 			LOGFMTD("not a complete data packet [cache_size:%d head_size:%d]", cache_size, C_NETHEAD_SIZE);
+// 		}
 	}
 	
-// 	clock_t t1 = clock();
-// 	{
-// 		LOGFMTD("NetCore::parse time [%f]", ((double)t1 - t) / CLOCKS_PER_SEC);
-// 	}
+	clock_t t1 = clock();
+	{
+		LOGFMTD("NetCore::parse time [%f]", ((double)t1 - t) / CLOCKS_PER_SEC);
+	}
 
 	return 0;
 }
